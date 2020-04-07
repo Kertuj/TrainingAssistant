daff3183162e2487a504d78331fc9b3eeb14ef98
Kertuj <1dojutrek@gmail.com>
2020-04-07 15:42:38 +0200

    Połaczenie z firebasem

--- app/src/main/java/com/example/trainingassistant/MainActivity.kt
+++ app/src/main/java/com/example/trainingassistant/MainActivity.kt
@@ -2,18 +2,37 @@ package com.example.trainingassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.trainingassistant.databinding.ActivityMainBinding
-import com.example.trainingassistant.ui.network.ApiClient
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-
-    private val api by lazy {
-        ApiClient.create()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("/sports/sportCollection")
+
+        myRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.value
+                Log.d("TAG", "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException())
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/trainingassistant/ui/network/Model.kt
+++
@@ -1,5 +0,0 @@
-package com.example.trainingassistant.ui.network
-
-object Model{
-
-}
